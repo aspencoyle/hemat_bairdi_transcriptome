8d849114d05f22e345537f268cb1face52aadec0
afcoyle <acoyle907@gmail.com>
2021-01-31 20:15:25 -0800

    continuing GO-MWU setup

---
+++ scripts/07_running_GO-MWU.R
@@ -0,0 +1,12 @@
+################################
+# 07_Running_GO-MWU
+# Aidan Coyle, afcoyle@uw.edu
+# 2021-01-31
+# Roberts lab, UW-SAFS
+################################
+
+# Install packages
+require(dichromat)
+
+# Import gene background
+geneBackground <- read.csv()
